6b94b680d1422acbd8f7e62d96f07a7df1881455
swifteye1111 <hasak.josh@gmail.com>
2022-09-07 17:12:32 +0100

    Remove extra end statement

--- substrings.rb
+++ substrings.rb
@@ -19,6 +19,6 @@ def count_em(str, substr)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-p substrings("below", dictionary)
-p substrings("Howdy partner, sit down! How's it going?", dictionary)
-end
+
+# substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
